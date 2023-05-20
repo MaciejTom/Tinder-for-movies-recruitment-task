bbcb851d9d48ddb12dc780a48e1d912ab6cd2782
Maciej Tomaszewicz <1994tomaszewicz@gmail.com>
2023-05-20 18:51:21 +0200

    Github pages configuration added

--- src/api/fakeBackend.ts
+++ src/api/fakeBackend.ts
@@ -5,11 +5,11 @@ export function makeServer() {
   const server = createServer({
     routes() {
       this.get("/recommendations", () => moviesList);
-      this.put("/recommendations/:id/accept", (schema, request) => {
+      this.put("/recommendations/:id/accept", (_schema, request) => {
         const id = request.params.id;
         return {id};
       });
-      this.put("/recommendations/:id/reject", (schema, request) => {
+      this.put("/recommendations/:id/reject", (_schema, request) => {
         const id = request.params.id;
         return {id};
       });
